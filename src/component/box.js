7a8c2702c4b02f1831431e1d5e5978673bf9afc8
Shibu Thannikkunnath <shibu@hotmail.co.in>
2018-09-13 10:23:01 -0400

    Fixed incorrect styling

--- src/component/box.js
+++ src/component/box.js
@@ -4,8 +4,8 @@ import './box.css';
 class Box extends React.Component {
     render(){
         return(
-            <div className="wrapper">
-                <div style={{backgroundColor:`${this.props.color}`}} className="box">
+            <div className="wrapper" style={{backgroundColor:`${this.props.color}`}}>
+                <div className="box">
                     <button className="boxy-button" onClick={()=>this.props.handleClick()}>Change color</button>
                 </div>
             </div>
